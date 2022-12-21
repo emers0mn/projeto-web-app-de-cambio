2a29b7a19689c54adc30735cd0b3f189d79ce8b3
emers0mn <emeron0000@gmail.com>
2022-12-21 01:10:37 -0300

    atualização de js

---
+++ script.js
@@ -0,0 +1,21 @@
+var cotacaoWu = document.getElementById("cotacaoWu");
+var real = document.getElementById("real");
+var peso = document.getElementById("peso");
+var calculo = document.getElementById("calcular");
+
+real.addEventListener('input', calcularPeso);
+peso.addEventListener('input',calcularReal);
+
+function calcularPeso() {
+    
+    let resultado =  real.value / cotacaoWu.value;
+    peso.value = resultado.toFixed(4)
+    return console.log(resultado)
+}
+
+function calcularReal() {
+    
+    let resultado =  peso.value / cotacaoWu.value;
+    real.value = resultado.toFixed(4)
+    return console.log(resultado)
+}
