9038fbac7291c1ac1f9b7ac4b3fb722d2a294975
emers0mn <emeron0000@gmail.com>
2023-07-13 22:27:41 -0300

    Calculo e comentários
    Nessa atualização eu coloquei os calculo e os comentários que estavam faltando.

--- script.js
+++ script.js
@@ -2,16 +2,26 @@ var cotacaoWu = document.getElementById("cotacaoWu");
 var real = document.getElementById("real");
 var peso = document.getElementById("peso");
 var calculo = document.getElementById("calcular");
+var apagarTudo = document.getElementById("apagar")
 var valoresFinais = document.getElementsByClassName("valores-finais");
 var coT = document.getElementById("coT");
 var coST = document.getElementById("coST");
 var modo = document.getElementById("modo");
 var geral = document.getElementsByClassName("content")[0];
 
+
 real.addEventListener('input', calcularPeso);
 peso.addEventListener('input',calcularReal);
 
-calculo.addEventListener('click', calculoTotal)
+
+calculo.addEventListener('click', calculoTotal);
+
+apagarTudo.addEventListener('click', () => {
+
+    peso.value = 0;
+    real.value = 0;
+    calculoTotal() = 0
+});
 
 function calcularPeso() {
     
@@ -37,21 +47,27 @@ function calculoTotal() {
     var taxaWu = 0
 
     if (realF <= 250) {
-        taxaWu = 9.9;
+        taxaWu = 10;
         valoresFinais[2].innerHTML = "R$" + taxaWu 
-    }else if (realF <= 500) {
+    }else if (realF <= 600) {
         taxaWu = 20;
         valoresFinais[2].innerHTML = "R$" + taxaWu 
-    } else if (realF <= 1500){
+    } else if (realF <= 1099){
         taxaWu = 30
         valoresFinais[2].innerHTML = "R$" + taxaWu 
     } else if (realF <= 2000) {
-        taxaWu = 50
+        taxaWu = 40
         valoresFinais[2].innerHTML = "R$" + taxaWu 
-    } else if (realF > 5000) {
-        taxaWu = 75
+    } else if (realF <= 3000) {
+        taxaWu = 60
         valoresFinais[2].innerHTML = "R$" + taxaWu 
-    }
+    } else if (realF <= 4000) {
+        taxaWu = 70
+        valoresFinais[2].innerHTML = "R$" + taxaWu 
+    } else if (realF <= 5000) {
+        taxaWu = 70
+        valoresFinais[2].innerHTML = "R$" + taxaWu
+    };
 
     let iof = (realF / 100)*1.1
     valoresFinais[3].innerHTML = "R$" + iof.toFixed(2)
